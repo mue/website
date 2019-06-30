9b854878ca37ce4687b9279f05ab745b693033ff
Turbo Marshmello <turbomarshmello@gmail.com>
2019-06-30 19:04:05 +0100

    Yes, stuff is here

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
+if (window.chrome) {
+  const a = document.getElementById("cheese");
+  a.setAttribute("href", "https://chrome.google.com/webstore/detail/mue/bngmbednanpcfochchhgbkookpiaiaid/related");
+  //document.getElementById('browser').classList.add('fab fa-chrome');
+}
+
+if (typeof InstallTrigger !== 'undefined') {
+  //document.getElementById('browser').classList.add('fab fa-firefox');
+}
