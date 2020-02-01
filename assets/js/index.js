b02fa18d8752c55f07cc6e81f19bed85ff24e1ef
David Ralph <ohlookitsderpy@protonmail.com>
2020-02-01 14:09:09 +0000

    fix navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,29 +4,3 @@ const navbar = () => {
     if (navbar.className === 'navbar') navbar.className += ' responsive';
     else navbar.className = 'navbar';
 }
-
-// Detect Browser
-const ua = detect.parse(navigator.userAgent);
-const button = document.getElementById('download');
-
-switch (ua.browser.family) {
-    case 'Chrome': {
-        button.href = 'webstore';
-        button.innerHTML = '<i class="fab fa-chrome"></i> Chrome/Chromium';
-        break;
-    }
-
-    case 'Firefox': {
-        button.href = 'webstore';
-        button.innerHTML = '<i class="fab fa-firefox"></i> Firefox';
-        break;
-    }
-
-    default: {
-        button.href = 'https://github.com/mue/mue';
-        button.innerHTML = '<i class="fa fa-question"</i> Other';
-    }
-}
-
-// Iframe Refresh
-const refreshIframe = () => document.getElementById('iframe').src = 'https://mue.now.sh';
