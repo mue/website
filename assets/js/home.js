25956d0addad4f9f6b785e2ccf403ee47a47a6b7
David Ralph <ohlookitsderpy@protonmail.com>
2020-02-19 14:15:30 +0000

    How did I miss this???

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,13 +4,13 @@ const button = document.getElementById('download');
 
 switch (ua.browser.family) {
     case 'Chrome': {
-        button.href = 'webstore';
+        button.href = 'https://chrome.google.com/webstore/detail/mue/bngmbednanpcfochchhgbkookpiaiaid';
         button.innerHTML = '<i class="fab fa-chrome"></i> Chrome/Chromium';
         break;
     }
 
     case 'Firefox': {
-        button.href = 'webstore';
+        button.href = 'https://addons.mozilla.org/en-GB/firefox/addon/mue/';
         button.innerHTML = '<i class="fab fa-firefox"></i> Firefox';
         break;
     }
