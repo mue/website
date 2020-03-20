324bdfa610ad172d7d20dd52e455bca8472bb046
David Ralph <ohlookitsderpy@protonmail.com>
2020-03-20 19:06:10 +0000

    easter egg lol

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,8 @@ const navbar = () => {
     if (navbar.className === 'navbar') navbar.className += ' responsive';
     else navbar.className = 'navbar';
 }
+
+// Easter Egg
+new Egg('up, up, down, down, left, right, left, right, b, a', () => {
+    window.location = 'https://www.youtube.com/watch?v=QZXc39hT8t4'; // yes
+}).listen();
