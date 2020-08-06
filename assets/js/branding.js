b612e1413e92d8b2669f010e534a2985789e86ed
David Ralph <ohlookitsderpy@protonmail.com>
2020-08-06 14:52:26 +0100

    new site

--- assets/js/branding.js
+++ assets/js/branding.js
@@ -1,1 +1,17 @@
 const downloadFile = (url) => download(url);
+
+const copyHex = (input) => {
+    let copy = document.createElement('input');
+    copy.type = 'text';
+    copy.value = input;
+    copy.id = 'copy';
+    document.body.appendChild(copy);
+    let text = document.getElementById('copy');
+    text.select();
+    document.execCommand('copy');
+    document.body.removeChild(copy);
+
+    const toast = document.getElementById('toast');
+    toast.className = 'show';
+    setTimeout(() => { toast.className = toast.className.replace('show', ''); }, 3000);
+}
