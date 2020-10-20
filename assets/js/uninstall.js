ef4917592c4841a6e322534bb26bf63e40e686af
David Ralph <davetheprogrammer2@gmail.com>
2020-10-20 10:17:26 +0100

    Updates

--- assets/js/uninstall.js
+++ assets/js/uninstall.js
@@ -1,5 +1,5 @@
 const validateForm = () => {
-    let form = document.forms['form'];
+    const form = document.forms['form'];
     if (form['MultiLine'].value === '') return document.getElementById('messageWarning').textContent = 'Message box must be filled';
     else document.getElementById('messageWarning').textContent = '';
     document.forms['form'].submit();
