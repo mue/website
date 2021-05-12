3cb000804b30e1158e1bea59074361f231596208
David Ralph <ohlookitsderpy@protonmail.com>
2021-05-12 14:27:06 +0100

    updates

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,15 +1,27 @@
 const validateForm = () => {
   const form = document.forms['form'];
-  if (!validator.isEmail(form['Email'].value)) document.getElementById('emailWarning').textContent = 'Invalid email';
-  else document.getElementById('emailWarning').textContent = '';
-  if (form['MultiLine'].value === '') return document.getElementById('messageWarning').textContent = 'Message box must be filled';
-  else document.getElementById('messageWarning').textContent = '';
+  const emailWarning = document.getElementById('emailWarning');
+  const messageWarning = document.getElementById('messageWarning');
+
+  if (!validator.isEmail(form['Email'].value)) {
+    emailWarning.textContent = 'Invalid email';
+  } else {
+    emailWarning.textContent = '';
+  }
+
+  if (form['MultiLine'].value === '') {
+    return messageWarning.textContent = 'Message box must be filled';
+  } else {
+    messageWarning.textContent = '';
+  }
+
   document.forms['form'].submit();
 }
 
 const toggleAnswer = (number) => {
   const answer = document.getElementById('answer' + number);
   const button = document.getElementById('dwnBtn' + number);
+
   if (answer.style.display === 'none') {
     answer.style.display = 'block';
     button.style.transform = 'rotate(180deg)';
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,11 @@ const navbar = () => {
     (navbar.className === 'navbar') ? navbar.className += ' responsive' : navbar.className = 'navbar';
 }
 
-// Easter Egg
-new Egg('up, up, down, down, left, right, left, right, b, a', () => {
-    window.location = 'https://www.youtube.com/watch?v=QZXc39hT8t4'; // yes
-}).listen();
-
-mybutton = document.getElementById('scroll');
+const mybutton = document.getElementById('scroll');
 
 window.onscroll = () => scrollFunction();
 
-const scrollFunction = () => (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? mybutton.style.display = "block" : mybutton.style.display = "none";
+const scrollFunction = () => (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? mybutton.style.display = 'block' : mybutton.style.display = 'none';
 
 const topFunction = () => {
   document.body.scrollTop = 0;
