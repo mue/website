46a8e1ec83ce708253be7bf746b44c33b66d0c94
David Ralph <ohlookitsderpy@protonmail.com>
2021-07-01 10:00:17 +0100

    Detect Edge and Whale

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,12 @@
 // Detect Browser
-const ua = detect.parse(navigator.userAgent);
+const ua = new UAParser(navigator.userAgent);
 const button = document.getElementById('download');
 
-if (ua.device.type === 'Mobile') {
+if (ua.getDevice() === 'Mobile' || ua.getDevice() === 'Tablet') {
     document.getElementById('unsupported').style.display = 'block';
     button.href = '';
     button.innerHTML = 'View Supported Browsers';
-} else switch (ua.browser.family) {
+} else switch (ua.getBrowser().name) {
     case 'Chrome':
         button.href = 'https://chrome.google.com/webstore/detail/mue/bngmbednanpcfochchhgbkookpiaiaid';
         button.innerHTML = '<i class="fab fa-chrome"></i> Add to Chrome';
@@ -15,6 +15,16 @@ if (ua.device.type === 'Mobile') {
     case 'Firefox':
         button.href = 'https://addons.mozilla.org/firefox/addon/mue/';
         button.innerHTML = '<i class="fab fa-firefox"></i> Add to Firefox';
+        break;
+
+    case 'Edge':
+        button.href = 'https://microsoftedge.microsoft.com/addons/detail/mue/aepnglgjfokepefimhbnibfjekidhmja';
+        button.innerHTML = '<i class="fab fa-edge"></i> Add to Edge';
+        break;
+
+    case 'Whale':
+        button.href = 'https://store.whale.naver.com/detail/ecllekeilcmicbfkkiknfdddbogibbnc';
+        button.innerHTML = '<i class="fas fa-plus"></i> Add to Whale';
         break;
 
     default:
