0e7f48ec9c9ee5c26ccbdfd4ffe9141d916a7051
David Ralph <me@davidjcralph.co.uk>
2021-08-14 14:09:58 +0100

    Uninstall form

--- assets/js/uninstall.js
+++ assets/js/uninstall.js
@@ -1,12 +1,32 @@
+document.getElementById('reason').onchange = (e) => {
+    document.getElementById('reasonWarning').textContent = '';
+    if (e.target.value === 'other') {
+        document.getElementById('otherReason').style.display = 'block';
+    } else {
+        document.getElementById('otherReason').style.display = 'none';
+    }
+}
+
+document.getElementById('likelyToTry').oninput = () => {
+    document.getElementById('likelyValue').innerText = '(' + document.getElementsByClassName('range')[0].value + ')';
+}
+
 const validateForm = () => {
     const form = document.forms['form'];
-    const messageWarning = document.getElementById('messageWarning');
-
-    if (form['MultiLine'].value === '') {
-        return messageWarning.textContent = 'Message box must be filled';
+    const reasonWarning = document.getElementById('reasonWarning');
+    const betterWarning = document.getElementById('betterWarning');
+  
+    if (document.getElementById('reason').value === 'other' && document.getElementById('otherReason').value === '') {
+      return reasonWarning.textContent = 'Reason must be provided';
     } else {
-        messageWarning.textContent = '';
+      reasonWarning.textContent = '';
     }
-
+  
+    if (form['MultiLine'].value === '') {
+      return betterWarning.textContent = 'Message box must be filled';
+    } else {
+      betterWarning.textContent = '';
+    }
+  
     document.forms['form'].submit();
 }
