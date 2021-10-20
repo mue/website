bf5122db1300b870bb6670c88ce912dd49c08f11
David Ralph <me@davidjcralph.co.uk>
2021-10-20 11:07:01 +0100

    5.3.3 is on edge now

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,32 +1,33 @@
 // Detect Browser
 const ua = new UAParser(navigator.userAgent);
 const button = document.getElementById('download');
+
 if (ua.getDevice().type === 'mobile' || ua.getDevice().type === 'tablet') {
-    document.getElementById('unsupported').style.display = 'block';
-    button.href = 'https://github.com/mue/mue#installation';
-    button.innerHTML = 'View Supported Browsers';
+  document.getElementById('unsupported').style.display = 'block';
+  button.href = 'https://github.com/mue/mue#installation';
+  button.innerHTML = 'View Supported Browsers';
 } else switch (ua.getBrowser().name) {
-    case 'Chrome':
-        button.href = 'https://chrome.google.com/webstore/detail/mue/bngmbednanpcfochchhgbkookpiaiaid';
-        button.innerHTML = '<i class="fab fa-chrome"></i> Add to Chrome';
-        break;
+  case 'Chrome':
+    button.href = 'https://chrome.google.com/webstore/detail/mue/bngmbednanpcfochchhgbkookpiaiaid';
+    button.innerHTML = '<i class="fab fa-chrome"></i> Add to Chrome';
+    break;
 
-    case 'Firefox':
-        button.href = 'https://addons.mozilla.org/firefox/addon/mue/';
-        button.innerHTML = '<i class="fab fa-firefox"></i> Add to Firefox';
-        break;
+  case 'Firefox':
+    button.href = 'https://addons.mozilla.org/firefox/addon/mue/';
+    button.innerHTML = '<i class="fab fa-firefox"></i> Add to Firefox';
+    break;
 
-    case 'Edge':
-        button.href = 'https://microsoftedge.microsoft.com/addons/detail/mue/aepnglgjfokepefimhbnibfjekidhmja';
-        button.innerHTML = '<i class="fab fa-edge"></i> Add to Edge';
-        break;
+  case 'Edge':
+    button.href = 'https://microsoftedge.microsoft.com/addons/detail/mue/aepnglgjfokepefimhbnibfjekidhmja';
+    button.innerHTML = '<i class="fab fa-edge"></i> Add to Edge';
+    break;
 
-    case 'Whale':
-        button.href = 'https://store.whale.naver.com/detail/ecllekeilcmicbfkkiknfdddbogibbnc';
-        button.innerHTML = '<i class="fas fa-plus"></i> Add to Whale';
-        break;
+  case 'Whale':
+    button.href = 'https://store.whale.naver.com/detail/ecllekeilcmicbfkkiknfdddbogibbnc';
+    button.innerHTML = '<i class="fas fa-plus"></i> Add to Whale';
+    break;
 
-    default:
-        button.href = 'https://github.com/mue/mue';
-        button.innerHTML = '<i class="fab fa-github"</i> Download from GitHub';
+  default:
+    button.href = 'https://github.com/mue/mue';
+    button.innerHTML = '<i class="fab fa-github"</i> Download from GitHub';
 }
--- assets/js/uninstall.js
+++ assets/js/uninstall.js
@@ -1,32 +1,32 @@
 document.getElementById('reason').onchange = (e) => {
-    document.getElementById('reasonWarning').textContent = '';
-    if (e.target.value === 'other') {
-        document.getElementById('otherReason').style.display = 'block';
-    } else {
-        document.getElementById('otherReason').style.display = 'none';
-    }
+  document.getElementById('reasonWarning').textContent = '';
+  if (e.target.value === 'other') {
+    document.getElementById('otherReason').style.display = 'block';
+  } else {
+    document.getElementById('otherReason').style.display = 'none';
+  }
 }
 
 document.getElementById('likelyToTry').oninput = () => {
-    document.getElementById('likelyValue').innerText = '(' + document.getElementsByClassName('range')[0].value + ')';
+  document.getElementById('likelyValue').innerText = '(' + document.getElementsByClassName('range')[0].value + ')';
 }
 
 const validateForm = () => {
-    const form = document.forms['form'];
-    const reasonWarning = document.getElementById('reasonWarning');
-    const betterWarning = document.getElementById('betterWarning');
+  const form = document.forms['form'];
+  const reasonWarning = document.getElementById('reasonWarning');
+  const betterWarning = document.getElementById('betterWarning');
   
-    if (document.getElementById('reason').value === 'other' && document.getElementById('otherReason').value === '') {
-      return reasonWarning.textContent = 'Reason must be provided';
-    } else {
-      reasonWarning.textContent = '';
-    }
+  if (document.getElementById('reason').value === 'other' && document.getElementById('otherReason').value === '') {
+    return reasonWarning.textContent = 'Reason must be provided';
+  } else {
+    reasonWarning.textContent = '';
+  }
   
-    if (form['MultiLine'].value === '') {
-      return betterWarning.textContent = 'Message box must be filled';
-    } else {
-      betterWarning.textContent = '';
-    }
+  if (form['MultiLine'].value === '') {
+    return betterWarning.textContent = 'Message box must be filled';
+  } else {
+    betterWarning.textContent = '';
+  }
   
-    document.forms['form'].submit();
+  document.forms['form'].submit();
 }
